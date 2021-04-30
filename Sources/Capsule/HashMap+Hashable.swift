b518d56b6518de90b2547145f7be341a4dc10af3
Michael Steindorfer <msteindorfer@apple.com>
2021-04-30 09:23:45 +0200

    [Capsule] Implement `Hashable` extension for `HashMap`

--- Sources/Capsule/HashMap+Hashable.swift
+++ Sources/Capsule/HashMap+Hashable.swift
@@ -2,19 +2,23 @@
 //
 // This source file is part of the Swift Collections open source project
 //
-// Copyright (c) 2019 - 2020 Apple Inc. and the Swift project authors
+// Copyright (c) 2019 - 2021 Apple Inc. and the Swift project authors
 // Licensed under Apache License v2.0 with Runtime Library Exception
 //
 // See https://swift.org/LICENSE.txt for license information
 //
 //===----------------------------------------------------------------------===//
 
-extension HashMap : Hashable {
-    public var hashValue: Int {
-        preconditionFailure("Not yet implemented")
-    }
-
-    public func hash(into: inout Hasher) {
-        preconditionFailure("Not yet implemented")
+// TODO settle on (commutative) hash semantics that is reconcilable with `cachedKeySetHashCode`
+extension HashMap : Hashable where Value : Hashable {
+    public func hash(into hasher: inout Hasher) {
+        var commutativeHash = 0
+        for (k, v) in self {
+            var elementHasher = Hasher()
+            elementHasher.combine(k)
+            elementHasher.combine(v)
+            commutativeHash ^= elementHasher.finalize()
+        }
+        hasher.combine(commutativeHash)
     }
 }
--- Tests/CapsuleTests/CapsuleTests.swift
+++ Tests/CapsuleTests/CapsuleTests.swift
@@ -2,7 +2,7 @@
 //
 // This source file is part of the Swift Collections open source project
 //
-// Copyright (c) 2019 - 2020 Apple Inc. and the Swift project authors
+// Copyright (c) 2019 - 2021 Apple Inc. and the Swift project authors
 // Licensed under Apache License v2.0 with Runtime Library Exception
 //
 // See https://swift.org/LICENSE.txt for license information
@@ -49,5 +49,32 @@ final class CapsuleTests: CollectionTestCase {
         expectEqual(copyAndSetTest.count, 0)
         expectEqual(copyAndSetTest[1], nil)
         expectEqual(copyAndSetTest[2], nil)
+    }
+
+    private func hashPair<Key : Hashable, Value : Hashable>(_ k: Key, _ v: Value) -> Int {
+        var hasher = Hasher()
+        hasher.combine(k)
+        hasher.combine(v)
+        return hasher.finalize()
+    }
+
+    func testHashable() {
+        let copyAndSetTest: HashMap<Int, String> =
+            [ 1 : "a", 2 : "b" ]
+
+        let hashPair1 = hashPair(1, "a")
+        let hashPair2 = hashPair(2, "b")
+
+        var commutativeHasher = Hasher()
+        commutativeHasher.combine(hashPair1 ^ hashPair2)
+
+        let expectedHashValue = commutativeHasher.finalize()
+
+        expectEqual(copyAndSetTest.hashValue, expectedHashValue)
+
+        var inoutHasher = Hasher()
+        copyAndSetTest.hash(into: &inoutHasher)
+
+        expectEqual(inoutHasher.finalize(), expectedHashValue)
     }
 }
