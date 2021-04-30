77ef6122d36c589e6f8ab3690d6bfe349bd3c6ec
Michael Steindorfer <msteindorfer@apple.com>
2021-04-30 09:38:17 +0200

    [Capsule] Rename and simplify smoke tests

--- Tests/CapsuleTests/CapsuleSmokeTests.swift
+++ Tests/CapsuleTests/CapsuleSmokeTests.swift
@@ -12,43 +12,40 @@
 import _CollectionsTestSupport
 @_spi(Testing) import Capsule
 
-final class CapsuleTests: CollectionTestCase {
+final class CapsuleSmokeTests: CollectionTestCase {
     func testSubscriptAdd() {
-        var copyAndSetTest: HashMap<Int, String> =
-            [ 1 : "a", 2 : "b" ]
+        var map: HashMap<Int, String> = [ 1 : "a", 2 : "b" ]
         
-        copyAndSetTest[3] = "x"
-        copyAndSetTest[4] = "y"
+        map[3] = "x"
+        map[4] = "y"
 
-        expectEqual(copyAndSetTest.count, 4)
-        expectEqual(copyAndSetTest[1], "a")
-        expectEqual(copyAndSetTest[2], "b")
-        expectEqual(copyAndSetTest[3], "x")
-        expectEqual(copyAndSetTest[4], "y")
+        expectEqual(map.count, 4)
+        expectEqual(map[1], "a")
+        expectEqual(map[2], "b")
+        expectEqual(map[3], "x")
+        expectEqual(map[4], "y")
     }
 
     func testSubscriptOverwrite() {
-        var copyAndSetTest: HashMap<Int, String> =
-            [ 1 : "a", 2 : "b" ]
+        var map: HashMap<Int, String> = [ 1 : "a", 2 : "b" ]
         
-        copyAndSetTest[1] = "x"
-        copyAndSetTest[2] = "y"
+        map[1] = "x"
+        map[2] = "y"
 
-        expectEqual(copyAndSetTest.count, 2)
-        expectEqual(copyAndSetTest[1], "x")
-        expectEqual(copyAndSetTest[2], "y")
+        expectEqual(map.count, 2)
+        expectEqual(map[1], "x")
+        expectEqual(map[2], "y")
     }
     
     func testSubscriptRemove() {
-        var copyAndSetTest: HashMap<Int, String> =
-            [ 1 : "a", 2 : "b" ]
+        var map: HashMap<Int, String> = [ 1 : "a", 2 : "b" ]
         
-        copyAndSetTest[1] = nil
-        copyAndSetTest[2] = nil
+        map[1] = nil
+        map[2] = nil
         
-        expectEqual(copyAndSetTest.count, 0)
-        expectEqual(copyAndSetTest[1], nil)
-        expectEqual(copyAndSetTest[2], nil)
+        expectEqual(map.count, 0)
+        expectEqual(map[1], nil)
+        expectEqual(map[2], nil)
     }
 
     private func hashPair<Key : Hashable, Value : Hashable>(_ k: Key, _ v: Value) -> Int {
@@ -59,8 +56,7 @@ final class CapsuleTests: CollectionTestCase {
     }
 
     func testHashable() {
-        let copyAndSetTest: HashMap<Int, String> =
-            [ 1 : "a", 2 : "b" ]
+        let map: HashMap<Int, String> = [ 1 : "a", 2 : "b" ]
 
         let hashPair1 = hashPair(1, "a")
         let hashPair2 = hashPair(2, "b")
@@ -70,10 +66,10 @@ final class CapsuleTests: CollectionTestCase {
 
         let expectedHashValue = commutativeHasher.finalize()
 
-        expectEqual(copyAndSetTest.hashValue, expectedHashValue)
+        expectEqual(map.hashValue, expectedHashValue)
 
         var inoutHasher = Hasher()
-        copyAndSetTest.hash(into: &inoutHasher)
+        map.hash(into: &inoutHasher)
 
         expectEqual(inoutHasher.finalize(), expectedHashValue)
     }
