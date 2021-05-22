3b14b3787faa798a256c2f0d4476731044b8e715
Amanuel Ephem <amanuelephrem776@gmail.com>
2021-05-22 00:54:20 -0400

    fixed formatting issue

--- Tests/PriorityQueueTests/PriorityQueueTests.swift
+++ Tests/PriorityQueueTests/PriorityQueueTests.swift
@@ -1,11 +1,16 @@
+//===----------------------------------------------------------------------===//
 //
-//  PriorityQueueTests.swift
-//  
+// This source file is part of the Swift Collections open source project
 //
+// Copyright (c) 2021 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
 //
+// See https://swift.org/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
+
 import XCTest
 import CollectionsTestSupport
-@_spi(Testing) import PriorityQueue
 import PriorityQueue
 
 final class PriorityQueueTests: CollectionTestCase {
