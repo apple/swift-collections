d05398adcefb6db348115cbec78961ac259a0542
Amanuel Ephem <amanuelephrem776@gmail.com>
2021-05-22 23:38:54 -0400

    Added conformance to the Sequence protocol

---
+++ Sources/PriorityQueue/PriorityQueue+Sequence.swift
@@ -0,0 +1,45 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Collections open source project
+//
+// Copyright (c) 2021 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
+
+extension PriorityQueue:Sequence{
+    
+    /*
+     *  Defines an iterator to conform to Sequence
+     */
+    public struct Iterator:IteratorProtocol{
+        //the priority queue at a specific moment
+        private var snapshotPriorityQueue:PriorityQueue
+        
+        public init(snapshotPriorityQueue:PriorityQueue){
+            self.snapshotPriorityQueue = snapshotPriorityQueue
+        }
+        
+        /*
+         *  Determines the next element in the iterator
+         *  @return  the next element in the iterator
+         */
+        public mutating func next() -> Element? {
+            return snapshotPriorityQueue.dequeue() ?? nil
+        }
+        
+    }
+    
+    /*
+     *  Creates the iterator for the priority queue
+     *  @return  the iterator for the priority queue
+     */
+    public func makeIterator() -> Iterator {
+        return Iterator(snapshotPriorityQueue: self)
+    }
+    
+    
+    
+}
