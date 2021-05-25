e69e47056cbf1e8d9189ef6ff4f0dbf26b392e26
MahanazHA <mhatiqullah@gmail.com>
2021-05-24 22:55:38 -0500

    Initial conformation to Collection. Lots of faulty code right now

--- Sources/BitArray/BitArray.swift
+++ Sources/BitArray/BitArray.swift
@@ -14,8 +14,40 @@ public struct BitArray {
     
 }
 
+
+// NOTE: Research @inlinable
+
 extension BitArray {
     public func test() {
-        print("Entered test function")
+        print("Nice! We're in test function!")
     }
+}
+
+extension BitArray: Collection {
+    public subscript(position: Int) -> Bool {
+        #warning("This is definitely wrong. Just have this here to get it to conform to collection for now")
+        return true
+    }
+    
+    public func index(after i: Int) -> Int {
+        #warning("Wth is this.")
+        return i
+    }
+    
+    public var startIndex: Int {
+        return 0
+    }
+    
+    public var endIndex: Int {
+        get {
+          return _getCount()
+        }
+    }
+    
+    internal func _getCount() -> Int {
+        #warning("fix this get count function to be, ummm, appropriate lol. Oh! And also try to see how this is internally gonna differ from an array. Especially considering the storage literally is an array.")
+        return self.storage.count // for now
+        //return _buffer.immutableCount
+    }
+    
 }
