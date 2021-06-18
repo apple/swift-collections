4f6471fa63198488457d6ac3ebde94639072f168
Tyler Stromberg <stromberg@squareup.com>
2021-06-18 15:26:08 -0700

    Fix code formatting in benchmarks

--- Benchmarks/Benchmarks/PriorityQueueBenchmarks.swift
+++ Benchmarks/Benchmarks/PriorityQueueBenchmarks.swift
@@ -14,89 +14,89 @@ import PriorityQueueModule
 import CppBenchmarks
 
 extension Benchmark {
-    public mutating func addPriorityQueueBenchmarks() {
-        self.addSimple(
-            title: "PriorityQueue<Int> init from range",
-            input: Int.self
-        ) { size in
-            blackHole(PriorityQueue(0..<size))
-        }
-
-        self.addSimple(
-            title: "PriorityQueue<Int> insert",
-            input: [Int].self
-        ) { input in
-            var queue = PriorityQueue<Int>()
-            for i in input {
-                queue.insert(i)
-            }
-            precondition(queue.count == input.count)
-            blackHole(queue)
-        }
-
-        self.add(
-            title: "PriorityQueue<Int> removeMax",
-            input: [Int].self
-        ) { input in
-            return { timer in
-                var queue = PriorityQueue(input)
-                while let max = queue.popMax() {
-                    blackHole(max)
-                }
-                precondition(queue.isEmpty)
-                blackHole(queue)
-            }
-        }
-
-        self.add(
-            title: "PriorityQueue<Int> removeMin",
-            input: [Int].self
-        ) { input in
-            return { timer in
-                var queue = PriorityQueue(input)
-                while let min = queue.popMin() {
-                    blackHole(min)
-                }
-                precondition(queue.isEmpty)
-                blackHole(queue)
-            }
-        }
+  public mutating func addPriorityQueueBenchmarks() {
+    self.addSimple(
+      title: "PriorityQueue<Int> init from range",
+      input: Int.self
+    ) { size in
+      blackHole(PriorityQueue(0..<size))
     }
+
+    self.addSimple(
+      title: "PriorityQueue<Int> insert",
+      input: [Int].self
+    ) { input in
+      var queue = PriorityQueue<Int>()
+      for i in input {
+        queue.insert(i)
+      }
+      precondition(queue.count == input.count)
+      blackHole(queue)
+    }
+
+    self.add(
+      title: "PriorityQueue<Int> removeMax",
+      input: [Int].self
+    ) { input in
+      return { timer in
+        var queue = PriorityQueue(input)
+        while let max = queue.popMax() {
+          blackHole(max)
+        }
+        precondition(queue.isEmpty)
+        blackHole(queue)
+      }
+    }
+
+    self.add(
+      title: "PriorityQueue<Int> removeMin",
+      input: [Int].self
+    ) { input in
+      return { timer in
+        var queue = PriorityQueue(input)
+        while let min = queue.popMin() {
+          blackHole(min)
+        }
+        precondition(queue.isEmpty)
+        blackHole(queue)
+      }
+    }
+  }
 }
 
 // MARK: -
 
 extension Benchmark {
-    public mutating func addCFBinaryHeapBenchmarks() {
-      self.addSimple(
-        title: "CFBinaryHeap insert",
-        input: [Int].self
-      ) { input in
+  public mutating func addCFBinaryHeapBenchmarks() {
+    self.addSimple(
+      title: "CFBinaryHeap insert",
+      input: [Int].self
+    ) { input in
+      let heap = BinaryHeap()
+      for i in input {
+        heap.insert(i)
+      }
+      precondition(heap.count == input.count)
+      blackHole(heap)
+    }
+
+    self.add(
+      title: "CFBinaryHeap removeMinimumValue",
+      input: [Int].self
+    ) { input in
+      return { timer in
         let heap = BinaryHeap()
         for i in input {
           heap.insert(i)
         }
-        precondition(heap.count == input.count)
+
+        while heap.count > 0 {
+          let min = heap.popMinimum()
+          blackHole(min)
+        }
+        precondition(heap.count == 0)
         blackHole(heap)
       }
-
-      self.add(
-        title: "CFBinaryHeap removeMinimumValue",
-        input: [Int].self
-      ) { input in
-        return { timer in
-          let heap = BinaryHeap()
-          for i in input {
-            heap.insert(i)
-          }
-
-          while heap.count > 0 {
-            let min = heap.popMinimum()
-            blackHole(min)
-          }
-          precondition(heap.count == 0)
-          blackHole(heap)
-        }
-      }
     }
+  }
 }
