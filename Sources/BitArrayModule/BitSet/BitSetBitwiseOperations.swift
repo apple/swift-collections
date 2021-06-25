6598bd47276061603719c7f36ad31027e65c3e42
MahanazHA <mhatiqullah@gmail.com>
2021-06-25 14:06:26 -0500

    Mutating Bitwise Operations
    Performance-UNfriendly starter implementations

--- Sources/BitArrayModule/BitSet/BitSetBitwiseOperations.swift
+++ Sources/BitArrayModule/BitSet/BitSetBitwiseOperations.swift
@@ -9,12 +9,6 @@ extension BitSet {
     
     public mutating func formBitwiseOR(with: BitSet) {
         
-        
-        
-    }
-    
-    public mutating func formBitwiseAND(with: BitSet) {
-        
         for i in with {
             if (!self.storage.contains(i)) {
                 self.append(i)
@@ -22,10 +16,25 @@ extension BitSet {
         }
         
         self.storage.sort()
+        
+    }
+    
+    public mutating func formBitwiseAND(with: BitSet) {
+        
+        self.storage.removeAll(where: {!with.contains($0)})
+        
     }
     
     public mutating func formBitwiseXOR(with: BitSet) {
+        for i in with {
+            if (!self.storage.contains(i)) {
+                self.append(i)
+            } else {
+                self.storage.removeAll(where: {$0 == i})
+            }
+        }
         
+        self.storage.sort()
     }
     
 }
