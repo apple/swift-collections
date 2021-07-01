7c2a75fdb22cdc63546323155081225b2ef2edf9
MahanazHA <mhatiqullah@gmail.com>
2021-07-01 15:05:36 -0500

    BitSet formUnion initial implementation

--- Sources/BitArrayModule/BitSet/BitSetBitwiseOperations.swift
+++ Sources/BitArrayModule/BitSet/BitSetBitwiseOperations.swift
@@ -7,6 +7,21 @@
 
 extension BitSet {
     
-
+    // in these implementations, the BitArray type storages do NOT have to be of same length, whereas for the current implementation of Bitwise Operations for BitArray, they do
+    public mutating func formUnion(with: BitSet) {
+        if (with.storage.storage.count < self.storage.storage.count) {
+            for i in 0..<with.storage.storage.count {
+                if (self.storage[i] || with.storage[i]) {
+                    self.storage[i] = true
+                }
+            }
+        } else {
+            for i in 0..<self.storage.storage.count {
+                if (self.storage[i] || with.storage[i]) {
+                    self.storage[i] = true
+                }
+            }
+        }
+    }
     
 }
