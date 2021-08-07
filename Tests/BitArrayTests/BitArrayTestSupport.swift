53d50f3d710900ead427b5c209ecd5079f3d41d2
MahanazHA <mhatiqullah@gmail.com>
2021-08-07 16:32:20 -0500

    add BitSet layout to test support

--- Tests/BitArrayTests/BitArrayTestSupport.swift
+++ Tests/BitArrayTests/BitArrayTestSupport.swift
@@ -37,6 +37,24 @@ func withSomeUsefulBoolArrays<C: Collection>(
   }
 }
 
+func withTheirBitSetLayout<C: Collection>(
+  _ label: String,
+  ofLayout layout: C,
+  file: StaticString = #file, line: UInt = #line,
+  _ body: ([Int]) throws -> Void
+) rethrows -> Void where C.Element == Bool, C.Index == Int {
+    var intArray: [Int] = []
+    for index in 0..<layout.endIndex {
+        if(layout[index]) {
+            intArray.append(index)
+        }
+    }
+    
+    let entry = TestContext.current.push("\(label): \(intArray)", file: file, line: line)
+    defer { TestContext.current.pop(entry) }
+    try body(intArray)
+}
+
 internal func _getSizes(_ bitWidth: Int) -> [Int] {
   var sizes: [Int] = []
   for i in 0...bitWidth+1 {
