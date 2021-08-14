07134a5bce472917a2fc2f2a19401861a688530b
Edward Marchant <ejmarchant@gmail.com>
2021-08-14 17:56:50 +0100

    Improve symbol documentation

--- Sources/OrderedCollections/OrderedDictionary/OrderedDictionary+Elements.swift
+++ Sources/OrderedCollections/OrderedDictionary/OrderedDictionary+Elements.swift
@@ -79,7 +79,7 @@ extension OrderedDictionary.Elements {
   /// into the dictionary that corresponds with the key-value pair.
   ///
   ///     let countryCodes: OrderedDictionary = ["BR": "Brazil", "GH": "Ghana", "JP": "Japan"]
-  ///     let index = countryCodes.index(forKey: "JP")
+  ///     let index = countryCodes.elements.index(forKey: "JP")
   ///
   ///     print("Country code for \(countryCodes[offset: index!].value): '\(countryCodes[offset: index!].key)'.")
   ///     // Prints "Country code for Japan: 'JP'."
