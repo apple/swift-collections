5ff8c003b0a353ca5b49a8b1b557474b17fd5a2a
ejmarchant <ejmarchant@gmail.com>
2021-08-16 21:37:05 +0100

    Fix documentation for types conforming to ExpressibleByArrayLiteral or ExpressibleByDictionaryLiteral (#82)
    # Conflicts:
    #	Sources/PriorityQueueModule/Heap+ExpressibleByArrayLiteral.swift

--- Sources/DequeModule/Deque+ExpressibleByArrayLiteral.swift
+++ Sources/DequeModule/Deque+ExpressibleByArrayLiteral.swift
@@ -13,12 +13,12 @@ extension Deque: ExpressibleByArrayLiteral {
   /// Creates a new deque from the contents of an array literal.
   ///
   /// Do not call this initializer directly. It is used by the compiler when
-  /// you use an array literal. Instead, create a new set using an array
+  /// you use an array literal. Instead, create a new deque using an array
   /// literal as its value by enclosing a comma-separated list of values in
-  /// square brackets. You can use an array literal anywhere a set is expected
+  /// square brackets. You can use an array literal anywhere a deque is expected
   /// by the type context.
   ///
-  /// - Parameter elements: A variadic list of elements of the new set.
+  /// - Parameter elements: A variadic list of elements of the new deque.
   @inlinable
   @inline(__always)
   public init(arrayLiteral elements: Element...) {
