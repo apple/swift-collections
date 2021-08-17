cd339541686c6841a0482cc8dd78c810cf86874c
Karoy Lorentey <klorentey@apple.com>
2021-08-16 22:28:55 -0700

    [Heap] Performance tweaks (#78)
    * [Heap] Don't always inline large functions
    
    * [docs] Use a stable link to the Atkinson article
    
    (The original link was pointing to course materials at a random university.)
    
    * Remove stray import
    
    * Revert "[Heap] Don't always inline large functions"
    
    This reverts commit 07ffd6591f2a790f6b84b668c06a188325d4eaf2.
    
    * [Heap] Enable code coverage collection in Xcode scheme
    
    * [Heap] Speed up invariant checking
    
    * [Heap] Precalculate levels for each offset
    
    `_Node` is a new struct that consists of a storage offset (the old index) along with its level in the tree. The level can be incrementally calculated, saving some time vs counting bits whenever it's needed.
    
    * [Heap] Switch to using unsafe buffer pointers
    
    Introduce `Heap._UnsafeHandle` (a thin wrapper around an unsafe buffer pointer) and rebase most heap algorithms on top of that instead of array operations.
    
    This simplifies things by reducing (hopefully) unnecessary index validation, resulting in some measurable performance improvements.
    
    * [Heap] Stop force-inlining bubbleUp; mark it releasenone
    
    Not inlining such a large function speeds things up by leaving some headroom for the optimizer to make better inlining decisions elsewhere. (Force inlining this resulted in the compiler not inlining the closure passed to `_update` instead, which isn't a great tradeoff.
    
    To speed things up, mark `bubbleUp` with `@_effects(releasenone)`. This may be questionable (because it calls `Element.<`), but it results in better codegen, making `insert` match the performance of `std::priority_queue`.
    
    * [Heap] Rework removals
    
    * [Heap] Remove dead code
    
    * [Heap] Switch to using _ContiguousArray as storage
    
    * [Heap] insert<S>(contentsOf:): add fast path for count == 0 case
    
    * [Heap] Perf pass on trickleDown code paths
    
    This improves popMin/popMax (and the sequence initializer) by reviewing trickleDown and optimizing things:
    
    - Replace swapAt with a scheme where we keep a hold in the storage buffer
    - Slightly shorten min/max dependency chain in primary sink loop
    
    In exchange, we get even less readable code.
    
    * [Heap] bubbleUp: remove @_effects attribute
    
    This reintroduces retain/release operations for the empty array until https://github.com/apple/swift/pull/38898 lands.
    
    Mitigate the performance costs of this by refactoring code to reduce the size of the inlined bubbleUpMin/Max invocations.
    
    * [Heap] Finetune Heap.insert

--- Sources/PriorityQueueModule/Heap+Invariants.swift
+++ Sources/PriorityQueueModule/Heap+Invariants.swift
@@ -14,78 +14,41 @@ import Foundation
 
 extension Heap {
   #if COLLECTIONS_INTERNAL_CHECKS
-  /// Iterates through all the levels in the heap, ensuring that items in min
-  /// levels are smaller than all their descendants and items in max levels
-  /// are larger than all their descendants.
-  ///
-  /// The min-max heap indices are structured like this:
-  ///
-  /// ```
-  /// min               0
-  /// max        1             2
-  /// min      3   4         5     6
-  /// max     7 8 9 10     11 12 13 14
-  /// min    15...
-  /// ...
-  /// ```
-  ///
-  /// The iteration happens in depth-first order, so the descendants of the
-  /// element at index 0 are checked to ensure they are >= that element. This is
-  /// repeated for each child of the element at index 0 (inverting the
-  /// comparison at each level).
-  ///
-  /// In the case of 7 elements total (spread across 3 levels), the checking
-  /// happens in the following order:
-  ///
-  /// ```
-  /// compare >= 0: 1, 3, 4, 2, 5, 6
-  /// compare <= 1: 3, 4
-  /// compare >= 3: (no children)
-  /// compare >= 4: (no children)
-  /// compare <= 2: 5, 6
-  /// compare >= 5: (no children)
-  /// compare >= 6: (no children)
-  /// ```
+  /// Visits each item in the heap in depth-first order, verifying that the
+  /// contents satisfy the min-max heap property.
   @inlinable
   @inline(never)
   internal func _checkInvariants() {
     guard count > 1 else { return }
-    var indicesToVisit: [Int] = [0]
+    _checkInvariants(node: .root, min: nil, max: nil)
+  }
 
-    while let elementIdx = indicesToVisit.popLast() {
-      let element = _storage[elementIdx]
-
-      let isMinLevel = _minMaxHeapIsMinLevel(elementIdx)
-
-      var descendantIndicesToVisit = [Int]()
-
-      // Add the children of this element to the outer loop (as we want to check
-      // that they are >= or <= their descendants as well)
-      if let rightIdx = _rightChildIndex(of: elementIdx) {
-        descendantIndicesToVisit.append(rightIdx)
-        indicesToVisit.append(rightIdx)
+  @inlinable
+  internal func _checkInvariants(node: _Node, min: Element?, max: Element?) {
+    let value = _storage[node.offset]
+    if let min = min {
+      precondition(value >= min,
+                   "Element \(value) at \(node) is less than min \(min)")
+    }
+    if let max = max {
+      precondition(value <= max,
+                   "Element \(value) at \(node) is greater than max \(max)")
+    }
+    let left = node.leftChild()
+    let right = node.rightChild()
+    if node.isMinLevel {
+      if left.offset < count {
+        _checkInvariants(node: left, min: value, max: max)
       }
-      if let leftIdx = _leftChildIndex(of: elementIdx) {
-        descendantIndicesToVisit.append(leftIdx)
-        indicesToVisit.append(leftIdx)
+      if right.offset < count {
+        _checkInvariants(node: right, min: value, max: max)
       }
-
-      // Compare the current element against its descendants
-      while let idx = descendantIndicesToVisit.popLast() {
-        if isMinLevel {
-          precondition(element <= _storage[idx],
-            "Element '\(_storage[idx])' at index \(idx) should be >= '\(element)'")
-        } else {
-          precondition(element >= _storage[idx],
-            "Element '\(_storage[idx])' at index \(idx) should be <= '\(element)'")
-        }
-
-        if let rightIdx = _rightChildIndex(of: idx) {
-          descendantIndicesToVisit.append(rightIdx)
-        }
-        if let leftIdx = _leftChildIndex(of: idx) {
-          descendantIndicesToVisit.append(leftIdx)
-        }
+    } else {
+      if left.offset < count {
+        _checkInvariants(node: left, min: min, max: value)
+      }
+      if right.offset < count {
+        _checkInvariants(node: right, min: min, max: value)
       }
     }
   }
--- Sources/PriorityQueueModule/Heap.swift
+++ Sources/PriorityQueueModule/Heap.swift
@@ -9,28 +9,36 @@
 //
 //===----------------------------------------------------------------------===//
 
-import Swift
-
 /// A [Min-Max Heap](https://en.wikipedia.org/wiki/Min-max_heap) data structure.
 ///
 /// In a min-max heap, each node at an even level in the tree is less than all
 /// its descendants, while each node at an odd level in the tree is greater than
 /// all of its descendants.
 ///
-/// The implementation is based off [Atkinson et al. Min-Max Heaps and
-/// Generalized Priority Queues (1986)](http://akira.ruc.dk/~keld/teaching/algoritmedesign_f03/Artikler/02/Atkinson86.pdf).
+/// The implementation is based on [Atkinson 1986]:
 ///
-/// M.D. Atkinson, J.-R. Sack, N. Santoro, T. Strothotte. October 1986.
-/// Min-Max Heaps and Generalized Priority Queues. Communications of the ACM.
-/// 29(10):996-1000.
+/// [Atkinson 1986]: https://doi.org/10.1145/6617.6621
+///
+/// M.D. Atkinson, J.-R. Sack, N. Santoro, T. Strothotte.
+/// "Min-Max Heaps and Generalized Priority Queues."
+/// *Communications of the ACM*, vol. 29, no. 10, Oct. 1986., pp. 996-1000,
+/// doi:[10.1145/6617.6621](https://doi.org/10.1145/6617.6621)
 public struct Heap<Element: Comparable> {
   @usableFromInline
-  internal var _storage: [Element]
+  internal var _storage: ContiguousArray<Element>
 
+  /// Creates an empty heap.
+  @inlinable
+  public init() {
+    _storage = []
+  }
+}
+
+extension Heap {
   /// A Boolean value indicating whether or not the heap is empty.
   ///
   /// - Complexity: O(1)
-  @inlinable
+  @inlinable @inline(__always)
   public var isEmpty: Bool {
     _storage.isEmpty
   }
@@ -38,7 +46,7 @@ public struct Heap<Element: Comparable> {
   /// The number of elements in the heap.
   ///
   /// - Complexity: O(1)
-  @inlinable
+  @inlinable @inline(__always)
   public var count: Int {
     _storage.count
   }
@@ -52,13 +60,7 @@ public struct Heap<Element: Comparable> {
   /// - Complexity: O(1)
   @inlinable
   public var unordered: [Element] {
-    _storage
-  }
-
-  /// Creates an empty heap.
-  @inlinable
-  public init() {
-    _storage = []
+    Array(_storage)
   }
 
   /// Inserts the given element into the heap.
@@ -67,7 +69,10 @@ public struct Heap<Element: Comparable> {
   @inlinable
   public mutating func insert(_ element: Element) {
     _storage.append(element)
-    _bubbleUp(elementAt: _storage.endIndex - 1)
+
+    _update { handle in
+      handle.bubbleUp(_Node(offset: handle.count - 1))
+    }
     _checkInvariants()
   }
 
@@ -84,17 +89,17 @@ public struct Heap<Element: Comparable> {
   /// - Complexity: O(1)
   @inlinable
   public func max() -> Element? {
-    switch _storage.count {
-    case 0, 1, 2:
-      // If count is 0, `last` will return `nil`
-      // If count is 1, the last (and only) item is the max
-      // If count is 2, the last item is the max (as it's the only item in the
-      // first max level)
-      return _storage.last
-    default:
+    _storage.withUnsafeBufferPointer { buffer in
+      guard buffer.count > 2 else {
+        // If count is 0, `last` will return `nil`
+        // If count is 1, the last (and only) item is the max
+        // If count is 2, the last item is the max (as it's the only item in the
+        // first max level)
+        return buffer.last
+      }
       // We have at least 3 items -- return the larger of the two in the first
       // max level
-      return Swift.max(_storage[1], _storage[2])
+      return Swift.max(buffer[1], buffer[2])
     }
   }
 
@@ -103,8 +108,20 @@ public struct Heap<Element: Comparable> {
   /// - Complexity: O(log `count`)
   @inlinable
   public mutating func popMin() -> Element? {
-    defer { _checkInvariants() }
-    return _remove(at: 0)
+    guard _storage.count > 0 else { return nil }
+
+    var removed = _storage.removeLast()
+
+    if _storage.count > 0 {
+      _update { handle in
+        let minNode = _Node.root
+        handle.swapAt(minNode, with: &removed)
+        handle.trickleDownMin(minNode)
+      }
+    }
+
+    _checkInvariants()
+    return removed
   }
 
   /// Removes and returns the element with the highest priority, if available.
@@ -112,19 +129,24 @@ public struct Heap<Element: Comparable> {
   /// - Complexity: O(log `count`)
   @inlinable
   public mutating func popMax() -> Element? {
-    defer { _checkInvariants() }
-    switch _storage.count {
-    case 0, 1, 2:
-      // If count is 0, `popLast` will return `nil`
-      // If count is 1, the last (and only) item is the max
-      // If count is 2, the last item is the max (as it's the only item in the
-      // first max level)
-      return _storage.popLast()
-    default:
-      // The max item is the larger of the two items in the first max level
-      let maxIdx = _storage[2] > _storage[1] ? 2 : 1
-      return _remove(at: maxIdx)
+    guard _storage.count > 2 else { return _storage.popLast() }
+
+    var removed = _storage.removeLast()
+
+    _update { handle in
+      if handle.count == 2 {
+        if handle[.leftMax] > removed {
+          handle.swapAt(.leftMax, with: &removed)
+        }
+      } else {
+        let maxNode = handle.maxValue(.rightMax, .leftMax)
+        handle.swapAt(maxNode, with: &removed)
+        handle.trickleDownMax(maxNode)
+      }
     }
+
+    _checkInvariants()
+    return removed
   }
 
   /// Removes and returns the element with the lowest priority.
@@ -146,358 +168,6 @@ public struct Heap<Element: Comparable> {
   public mutating func removeMax() -> Element {
     return popMax()!
   }
-
-  // MARK: -
-
-  @inline(__always)
-  @inlinable
-  internal mutating func _bubbleUp(elementAt index: Int) {
-    guard let parentIdx = _parentIndex(of: index) else {
-      // We're already at the root -- can't go any further
-      return
-    }
-
-    // Figure out if `index` is on an even or odd level
-    let levelIsMin = _minMaxHeapIsMinLevel(index)
-
-    if levelIsMin {
-      if _storage[index] > _storage[parentIdx] {
-        _swapAt(index, parentIdx)
-        _bubbleUpMax(elementAt: parentIdx)
-      } else {
-        _bubbleUpMin(elementAt: index)
-      }
-    } else {
-      if _storage[index] < _storage[parentIdx] {
-        _swapAt(index, parentIdx)
-        _bubbleUpMin(elementAt: parentIdx)
-      } else {
-        _bubbleUpMax(elementAt: index)
-      }
-    }
-  }
-
-  @inline(__always)
-  @inlinable
-  internal mutating func _bubbleUpMin(elementAt index: Int) {
-    var index = index
-
-    while let grandparentIdx = _grandparentIndex(of: index),
-          _storage[index] < _storage[grandparentIdx] {
-      _swapAt(index, grandparentIdx)
-      index = grandparentIdx
-    }
-  }
-
-  @inline(__always)
-  @inlinable
-  internal mutating func _bubbleUpMax(elementAt index: Int) {
-    var index = index
-
-    while let grandparentIdx = _grandparentIndex(of: index),
-          _storage[index] > _storage[grandparentIdx] {
-      _swapAt(index, grandparentIdx)
-      index = grandparentIdx
-    }
-  }
-
-  // MARK: -
-
-  @discardableResult
-  @inlinable
-  internal mutating func _remove(at index: Int) -> Element? {
-    guard _storage.count > index else {
-      return nil
-    }
-
-    var removed = _storage.removeLast()
-
-    if index < _storage.count {
-      swap(&removed, &_storage[index])
-      _trickleDown(elementAt: index)
-    }
-
-    return removed
-  }
-
-  // MARK: -
-
-  @inline(__always)
-  @inlinable
-  internal mutating func _trickleDown(elementAt index: Int) {
-    // Figure out if `index` is on an even or odd level
-    let levelIsMin = _minMaxHeapIsMinLevel(index)
-
-    if levelIsMin {
-      _trickleDownMin(elementAt: index)
-    } else {
-      _trickleDownMax(elementAt: index)
-    }
-  }
- 
-  @inline(__always)
-  @inlinable
-  internal mutating func _trickleDownMin(elementAt index: Int) {
-    var index = index
-
-    while let (smallestDescendantIdx, isChild) =
-            _indexOfLowestPriorityChildOrGrandchild(of: index) {
-      if _storage[smallestDescendantIdx] < _storage[index] {
-        _swapAt(smallestDescendantIdx, index)
-
-        if isChild {
-          return
-        }
-
-        // Smallest is a grandchild
-        let parentIdx = _parentIndex(of: smallestDescendantIdx)!
-        if _storage[smallestDescendantIdx] > _storage[parentIdx] {
-          _swapAt(smallestDescendantIdx, parentIdx)
-        }
-
-        index = smallestDescendantIdx
-      } else {
-        return
-      }
-    }
-  }
-
-  @inline(__always)
-  @inlinable
-  internal mutating func _trickleDownMax(elementAt index: Int) {
-    var index = index
-
-    while let (largestDescendantIdx, isChild) =
-            _indexOfHighestPriorityChildOrGrandchild(of: index) {
-      if _storage[largestDescendantIdx] > _storage[index] {
-        _swapAt(largestDescendantIdx, index)
-
-        if isChild {
-          return
-        }
-
-        // Largest is a grandchild
-        let parentIdx = _parentIndex(of: largestDescendantIdx)!
-        if _storage[largestDescendantIdx] < _storage[parentIdx] {
-          _swapAt(largestDescendantIdx, parentIdx)
-        }
-
-        index = largestDescendantIdx
-      } else {
-        return
-      }
-    }
-  }
-
-  /// Returns the lowest priority child or grandchild of the element at the
-  /// given index.
-  ///
-  /// Returns `nil` if the element has no descendants.
-  ///
-  /// - parameter index: The index of the element whose descendants should be
-  ///                    compared.
-  @inline(__always)
-  @inlinable
-  internal func _indexOfLowestPriorityChildOrGrandchild(
-    of index: Int
-  ) -> (index: Int, isChild: Bool)? {
-    guard let leftChildIdx = _leftChildIndex(of: index) else {
-      return nil
-    }
-
-    var result: (index: Int, isChild: Bool) = (leftChildIdx, true)
-
-    guard let rightChildIdx = _rightChildIndex(of: index) else {
-      return result
-    }
-
-    guard let firstGrandchildIdx = _firstGrandchildIndex(of: index),
-          let lastGrandchildIdx = _lastGrandchildIndex(of: index)
-    else {
-      // We have no grandchildren -- compare the two children instead
-      if _storage[rightChildIdx] < _storage[leftChildIdx] {
-        result.index = rightChildIdx
-      }
-
-      return result
-    }
-
-    // If we have 4 grandchildren, we can skip comparing the children as the
-    // heap invariants will ensure that the grandchildren will be smaller.
-    // Otherwise, we need to do the comparison.
-    if lastGrandchildIdx != firstGrandchildIdx + 3 {
-      // Compare the two children
-      if _storage[rightChildIdx] < _storage[leftChildIdx] {
-        result.index = rightChildIdx
-      }
-    }
-
-    // Iterate through the grandchildren
-    for i in firstGrandchildIdx...lastGrandchildIdx {
-      if _storage[i] < _storage[result.index] {
-        result.index = i
-        result.isChild = false
-      }
-    }
-
-    return result
-  }
-
-  /// Returns the highest priority child or grandchild of the element at the
-  /// given index.
-  ///
-  /// Returns `nil` if the element has no descendants.
-  ///
-  /// - parameter index: The index of the item whose descendants should be
-  ///                    compared.
-  @inline(__always)
-  @inlinable
-  internal func _indexOfHighestPriorityChildOrGrandchild(
-    of index: Int
-  ) -> (index: Int, isChild: Bool)? {
-    guard let leftChildIdx = _leftChildIndex(of: index) else {
-      return nil
-    }
-
-    var result: (index: Int, isChild: Bool) = (leftChildIdx, true)
-
-    guard let rightChildIdx = _rightChildIndex(of: index) else {
-      return result
-    }
-
-    guard let firstGrandchildIdx = _firstGrandchildIndex(of: index),
-          let lastGrandchildIdx = _lastGrandchildIndex(of: index)
-    else {
-      // We have no grandchildren -- compare the two children instead
-      if _storage[rightChildIdx] > _storage[leftChildIdx] {
-        result.index = rightChildIdx
-      }
-
-      return result
-    }
-
-    // If we have 4 grandchildren, we can skip comparing the children as the
-    // heap invariants will ensure that the grandchildren will be larger.
-    // Otherwise, we need to do the comparison.
-    if lastGrandchildIdx != firstGrandchildIdx + 3 {
-      // Compare the two children
-      if _storage[rightChildIdx] > _storage[leftChildIdx] {
-        result.index = rightChildIdx
-      }
-    }
-
-    // Iterate through the grandchildren
-    for i in firstGrandchildIdx...lastGrandchildIdx {
-      if _storage[i] > _storage[result.index] {
-        result.index = i
-        result.isChild = false
-      }
-    }
-
-    return result
-  }
-
-  // MARK: - Helpers
-
-  /// Returns `true` if `index` falls on a min level in a min-max heap.
-  ///
-  /// - Precondition: `index` must be non-negative.
-  @inline(__always)
-  @inlinable
-  internal func _minMaxHeapIsMinLevel(_ index: Int) -> Bool {
-    precondition(index >= 0)
-
-    return (index + 1)._binaryLogarithm() & 0b1 == 0
-  }
-
-  /// Swaps the elements in the heap at the given indices.
-  @inline(__always)
-  @inlinable
-  internal mutating func _swapAt(_ i: Int, _ j: Int) {
-    let tmp = _storage[i]
-    _storage[i] = _storage[j]
-    _storage[j] = tmp
-  }
-
-  /// Returns the parent index of the given `index`
-  /// or `nil` if the index has no parent (i.e. `index == 0`).
-  @inline(__always)
-  @inlinable
-  internal func _parentIndex(of index: Int) -> Int? {
-    guard index > 0 else {
-      return nil
-    }
-
-    return (index - 1) / 2
-  }
-
-  /// Returns the grandparent index of the given `index`
-  /// or `nil` if the index has no grandparent.
-  @inline(__always)
-  @inlinable
-  internal func _grandparentIndex(of index: Int) -> Int? {
-    guard index > 2 else {
-      return nil
-    }
-
-    return (index - 3) / 4
-  }
-
-  /// Returns the first child index of the given `index`
-  /// or `nil` if the index has no children.
-  @inline(__always)
-  @inlinable
-  internal func _leftChildIndex(of index: Int) -> Int? {
-    let childIdx = index * 2 + 1
-    guard childIdx < _storage.count else {
-      return nil
-    }
-
-    return childIdx
-  }
-
-  /// Returns the right child index of the given `index`
-  /// or `nil` if the index has no right child.
-  @inline(__always)
-  @inlinable
-  internal func _rightChildIndex(of index: Int) -> Int? {
-    let childIdx = index * 2 + 2
-    guard childIdx < _storage.count else {
-      return nil
-    }
-
-    return childIdx
-  }
-
-  /// Returns the first grandchild index of the given `index`
-  /// or `nil` if the index has no grandchildren.
-  @inline(__always)
-  @inlinable
-  internal func _firstGrandchildIndex(of index: Int) -> Int? {
-    let grandchildIdx = index * 4 + 3
-    guard grandchildIdx < _storage.count else {
-      return nil
-    }
-
-    return grandchildIdx
-  }
-
-  /// Returns the last valid grandchild index of the given `index`
-  /// or `nil` if the index has no grandchildren.
-  ///
-  /// In cases where the given index only has one grandchild, the index
-  /// returned by this function is the same as that returned by
-  /// `_firstGrandchildIndex`.
-  @inline(__always)
-  @inlinable
-  internal func _lastGrandchildIndex(of index: Int) -> Int? {
-    guard _firstGrandchildIndex(of: index) != nil else {
-      // There are no grandchildren of the node at `index`
-      return nil
-    }
-
-    return Swift.min(index * 4 + 6, _storage.count - 1)
-  }
 }
 
 // MARK: -
@@ -505,17 +175,20 @@ public struct Heap<Element: Comparable> {
 extension Heap {
   /// Initializes a heap from a sequence.
   ///
-  /// Utilizes [Floyd's linear-time heap construction algorithm](https://en.wikipedia.org/wiki/Heapsort#Floyd's_heap_construction).
-  ///
-  /// - Complexity: O(n), where `n` is the length of `elements`.
+  /// - Complexity: O(*n*), where *n* is the length of `elements`.
   @inlinable
   public init<S: Sequence>(_ elements: S) where S.Element == Element {
-    _storage = Array(elements)
+    // This is Floyd's linear-time heap construction algorithm.
+    // (https://en.wikipedia.org/wiki/Heapsort#Floyd's_heap_construction).
+    //
+    // FIXME: See if a more cache friendly algorithm would be faster.
 
-    for idx in (0 ..< (_storage.count / 2)).reversed() {
-      _trickleDown(elementAt: idx)
+    _storage = ContiguousArray(elements)
+    guard _storage.count > 1 else { return }
+
+    _update { handle in
+      handle.heapify()
     }
-
     _checkInvariants()
   }
 
@@ -523,9 +196,16 @@ extension Heap {
   ///
   /// - Parameter newElements: The new elements to insert into the heap.
   ///
-  /// - Complexity: O(n * log `count`), where `n` is the length of `newElements`.
+  /// - Complexity: O(*n* * log(`count`)), where *n* is the length of `newElements`.
   @inlinable
-  public mutating func insert<S: Sequence>(contentsOf newElements: S) where S.Element == Element {
+  public mutating func insert<S: Sequence>(
+    contentsOf newElements: S
+  ) where S.Element == Element {
+    if count == 0 {
+      self = Self(newElements)
+      return
+    }
+    _storage.reserveCapacity(count + newElements.underestimatedCount)
     for element in newElements {
       insert(element)
     }
