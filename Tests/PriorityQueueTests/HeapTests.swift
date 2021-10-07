cfb6dd1810c8a90fbe2f6bb877034acd781084c8
Sergey Petrenko <just.gull@gmail.com>
2021-10-07 11:46:25 +0300

    [Heap] Enable heap tests in optimized builds (#101)
    @testable import is replaced with @_spi(Testing) import.
    A test for _Node is moved to another file because this test needs access to internals of PriorityQueueModule and this test will not work in optimized builds.

--- Tests/PriorityQueueTests/HeapTests.swift
+++ Tests/PriorityQueueTests/HeapTests.swift
@@ -9,9 +9,8 @@
 //
 //===----------------------------------------------------------------------===//
 
-#if DEBUG
 import XCTest
-@testable import PriorityQueueModule
+@_spi(Testing) import PriorityQueueModule
 
 final class HeapTests: XCTestCase {
   func test_isEmpty() {
@@ -349,19 +348,6 @@ final class HeapTests: XCTestCase {
 
   // MARK: -
 
-  func test_levelCalculation() {
-    // Check alternating min and max levels in the heap
-    var isMin = true
-    for exp in 0...12 {
-      // Check [2^exp, 2^(exp + 1))
-      for offset in Int(pow(2, Double(exp)) - 1)..<Int(pow(2, Double(exp + 1)) - 1) {
-        let node = _Node(offset: offset)
-        XCTAssertEqual(node.isMinLevel, isMin)
-      }
-      isMin.toggle()
-    }
-  }
-
   func test_initializer_fromCollection() {
     var heap = Heap((1...20).shuffled())
     XCTAssertEqual(heap.max(), 20)
@@ -420,4 +406,3 @@ final class HeapTests: XCTestCase {
     }
   }
 }
-#endif
