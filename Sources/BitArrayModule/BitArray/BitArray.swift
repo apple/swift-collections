de01bac0005c59f7ad19093e9b3f6cc137c35108
MahanazAtiqullah <65631912+MahanazAtiqullah@users.noreply.github.com>
2021-10-07 12:36:22 -0500

    Documentation for bitwise operations and some initializers

--- Sources/BitArrayModule/BitArray/BitArray.swift
+++ Sources/BitArrayModule/BitArray/BitArray.swift
@@ -19,17 +19,30 @@ public struct BitArray: ExpressibleByArrayLiteral, Codable {
   public init<S>(_ elements: S) where S : Sequence, Bool == S.Element {
     storage.reserveCapacity(elements.underestimatedCount / WORD.bitWidth)
     for value in elements {
-        self.append(value)
+      self.append(value)
     }
   }
   
   public init(arrayLiteral elements: Bool...) {
     storage.reserveCapacity(elements.underestimatedCount / WORD.bitWidth)
     for value in elements {
-        self.append(value)
+      self.append(value)
     }
   }
-  
+  /// Creates a new collection containing the specified number of a single, repeated `Bool`
+  ///
+  /// The following example creates a BitArray initialized with 5 true values
+  ///
+  ///     let fiveTrues = BitArray(repeating: true, count: 5)
+  ///     print(fiveTrues)
+  ///     // Prints "BitArray(storage: [31], excess: 5)"
+  ///     print(Array(fiveTrues))
+  ///     // Prints "[true, true, true, true, true]"
+  ///
+  /// - Parameters:
+  ///   - repeatedValue: The  `Bool` to repeat.
+  ///   - count: The number of times to repeat the value passed in the
+  ///     `repeating` parameter. `count` must be zero or greater.
   public init(repeating repeatedValue: Bool, count: Int) {
     precondition(count >= 0, "Count must be greater than or equal to 0")
     if (count == 0) {
@@ -53,7 +66,18 @@ public struct BitArray: ExpressibleByArrayLiteral, Codable {
       excess = WORD(count%(WORD.bitWidth))
     }
   }
-  
+  /// Creates a new BitArray using a BitSet, where the indices in the BitSet become the true values in the BitArray. The resulting BitArray often contains many padded false values at the end from empty bits that fill up the word
+  ///
+  /// The following example creates a BitArray initialized by a BitSet
+  ///     let bitSet: BitSet = [0, 1, 3, 5]
+  ///     let bitArray = BitArray(bitSet)
+  ///     print(bitArray)
+  ///     // Prints "BitArray(storage: [43], excess: 0)"
+  ///     print(Array(bitArray))
+  ///     // Prints "[true, true, false, true, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]"
+  ///
+  /// - Parameters:
+  ///   - bitSet: An initialized BitSet
   public init(_ bitSet: BitSet) {
     storage = bitSet.storage.storage
     excess = bitSet.storage.excess
--- Sources/BitArrayModule/BitArray/BitArrayBitwiseOperations.swift
+++ Sources/BitArrayModule/BitArray/BitArrayBitwiseOperations.swift
@@ -11,7 +11,17 @@
 
 extension BitArray {
   
-  
+  /// mutates self by setting the value in each index to true if either self or the passed in BitArray -- or both -- have a true value in that respective index.
+  ///
+  /// The following example mutates a BitArray to be OR'd with another BitArray
+  ///     var bitArray: BitArray = [true, false, true, false]
+  ///     let secondBitArray = [true, false, false, true]
+  ///     bitArray.formBitwiseOr(secondBitArray)
+  ///     print(Array(bitArray))
+  ///     Prints "[true, false, true, true]"
+  ///
+  /// - Parameters:
+  ///   - other: An initialized BitArray. `other` must have the same count as `self`
   public mutating func formBitwiseOr(_ other: BitArray) {
     
     precondition(self.storage.count == other.storage.count, "Bitwise operations on BitArrays of different length is currently not supported")
@@ -20,7 +30,17 @@ extension BitArray {
       self.storage[i] |= other.storage[i]
     }
   }
-  
+  /// mutates self by setting the value in each index to true if both self and the passed in BitArray have a true value in that respective index.
+  ///
+  /// The following example mutates a BitArray to be AND'd with another BitArray
+  ///     var bitArray: BitArray = [true, false, true, false]
+  ///     let secondBitArray = [true, false, false, true]
+  ///     bitArray.formBitwiseAnd(secondBitArray)
+  ///     print(Array(bitArray))
+  ///     Prints "[true, false, false, false]"
+  ///
+  /// - Parameters:
+  ///   - other: An initialized BitArray. `other` must have the same count as `self`
   public mutating func formBitwiseAnd(_ other: BitArray) {
     
     precondition(self.storage.count == other.storage.count, "Bitwise operations on BitArrays of different length is currently not supported")
@@ -29,7 +49,17 @@ extension BitArray {
       self.storage[i] &= other.storage[i]
     }
   }
-  
+  /// mutates self by setting the value in each index to true if either self or the passed in BitArray -- but not both -- have a true value in that respective index.
+  ///
+  /// The following example mutates self to be XOR'd with another BitArray
+  ///     var bitArray: BitArray = [true, false, true, false]
+  ///     let secondBitArray = [true, false, false, true]
+  ///     bitArray.formBitwiseXor(secondBitArray)
+  ///     print(Array(bitArray))
+  ///     Prints "[false, false, true, true]"
+  ///
+  /// - Parameters:
+  ///   - other: An initialized BitArray. `other` must have the same count as `self`
   public mutating func formBitwiseXor(_ other: BitArray) {
     
     precondition(self.storage.count == other.storage.count, "Bitwise operations on BitArrays of different length is currently not supported")
@@ -38,7 +68,13 @@ extension BitArray {
       self.storage[i] ^= other.storage[i]
     }
   }
-  
+  /// mutates self by flipping each binary value in self
+  ///
+  /// The following example mutates self to its own NOT or inverted self
+  ///     var bitArray: BitArray = [true, false, true, false]
+  ///     bitArray.formBitwiseNot()
+  ///     print(Array(bitArray))
+  ///     Prints "[false, true, false, true]"
   public mutating func formBitwiseNot() {
     
     if (count == 0) {
@@ -60,25 +96,66 @@ extension BitArray {
     }
     
   }
-  
+  /// creates and returns a new BitArray by setting the value in each index of the new BitArray to true if either self or the passed in BitArray -- or both -- have a true value in that respective index.
+  ///
+  /// The following example creates and returns a BitArray that is the OR'd result of self and another BitArray
+  ///     let bitArray: BitArray = [true, false, true, false]
+  ///     let secondBitArray = [true, false, false, true]
+  ///     let newBitArray = bitArray.bitwiseOr(secondBitArray)
+  ///     print(Array(newBitArray))
+  ///     Prints "[true, false, true, true]"
+  ///
+  /// - Parameters:
+  ///   - other: An initialized BitArray. `other` must have the same count as `self`
+  /// - Returns: The BitArray that is the XOR'd result of `self` and `other`
   public func bitwiseOr(_ other: BitArray) -> BitArray{
     var copy = self
     copy.formBitwiseOr(other)
     return copy
   }
-  
+  /// creates and returns a new BitArray by setting the value in each index of the new BitArray to true if both self and the passed in BitArray have a true value in that respective index.
+  ///
+  /// The following example creates and returns a BitArray that is the AND'd result of self and another BitArray
+  ///     let bitArray: BitArray = [true, false, true, false]
+  ///     let secondBitArray = [true, false, false, true]
+  ///     let newBitArray = bitArray.bitwiseAnd(secondBitArray)
+  ///     print(Array(newBitArray))
+  ///     Prints "[true, false, false, false]"
+  ///
+  /// - Parameters:
+  ///   - other: An initialized BitArray. `other` must have the same count as `self`
+  /// - Returns: The BitArray that is the AND'd result of `self` and `other`
   public func bitwiseAnd(_ other: BitArray) -> BitArray{
     var copy = self
     copy.formBitwiseAnd(other)
     return copy
   }
-  
+  /// creates a new BitArray by setting the value in each index of the new BitArray to true if either self or the passed in BitArray -- but not both -- have a true value in that respective index.
+  ///
+  /// The following example creates a new a BitArray that is the XOR'd result of self and another BitArray
+  ///     let bitArray: BitArray = [true, false, true, false]
+  ///     let secondBitArray = [true, false, false, true]
+  ///     let newBitArray = bitArray.bitwiseOr(secondBitArray)
+  ///     print(Array(newBitArray))
+  ///     Prints "[false, false, true, true]"
+  ///
+  /// - Parameters:
+  ///   - other: An initialized BitArray. `other` must have the same count as `self`
+  /// - Returns: The BitArray that is the XOR'd result of `self` and `other`
   public func bitwiseXor(_ other: BitArray) -> BitArray{
     var copy = self
     copy.formBitwiseXor(other)
     return copy
   }
-  
+  /// creates a new BitArray by setting each value in the new BitArray to the opposite value of that in self
+  ///
+  /// The following example creates a new a BitArray that is the inverse of self
+  ///     let bitArray: BitArray = [true, false, true, false]
+  ///     let newBitArray = bitArray.bitwiseNot()
+  ///     print(Array(newBitArray))
+  ///     Prints "[false, true, false, true]"
+  ///
+  /// - Returns: The BitArray that contains the inverse or opposite values of that in self
   public func bitwiseNot() -> BitArray {
     var copy = self
     copy.formBitwiseNot()
@@ -89,39 +166,116 @@ extension BitArray {
 // Overloaded Operators
 
 extension BitArray {
-  
+  /// overloaded operator that creates and returns a new BitArray by setting the value in each index to true if either the lhs BitArray or the rhs BitArray -- or both -- have a true value in that respective index.
+  ///
+  /// The following example creates and returns a BitArray that is the OR'd result of the lhs and rhs
+  ///     let bitArray: BitArray = [true, false, true, false]
+  ///     let secondBitArray = [true, false, false, true]
+  ///     let newBitArray = bitArray | secondBitArray
+  ///     print(Array(newBitArray))
+  ///     Prints "[true, false, true, true]"
+  ///
+  /// - Parameters:
+  ///   - lhs: An initialized BitArray
+  ///   - rhs: An initialized BitArray. `rhs` must have same count as `lhs`
+  /// - Returns: The BitArray that is the XOR'd result of the lhs and rhs
   public static func | (lhs: BitArray, rhs: BitArray) -> BitArray {
     var copy = lhs
     copy.formBitwiseOr(rhs)
     return copy
   }
-  
+  /// overloaded operator that creates and returns a new BitArray by setting the value in each index of the new BitArray to true if both the lhs BitArray and the rhs BitArray have a true value in that respective index.
+  ///
+  /// The following example creates and returns a BitArray that is the AND'd result of self and another BitArray
+  ///     let bitArray: BitArray = [true, false, true, false]
+  ///     let secondBitArray = [true, false, false, true]
+  ///     let newBitArray = bitArray & secondBitArray
+  ///     print(Array(newBitArray))
+  ///     Prints "[true, false, false, false]"
+  ///
+  /// - Parameters:
+  ///   - lhs: An initialized BitArray
+  ///   - rhs: An initialized BitArray. `rhs` must have same count as `lhs`
+  /// - Returns: The BitArray that is the AND'd result of the lhs and rhs
   public static func & (lhs: BitArray, rhs: BitArray) -> BitArray {
     var copy = lhs
     copy.formBitwiseAnd(rhs)
     return copy
   }
-  
+  /// creates a new BitArray by setting the value in each index of the new BitArray to true if either lhs BitArray or the rhs BitArray -- but not both -- have a true value in that respective index.
+  ///
+  /// The following example creates a new a BitArray that is the XOR'd result of self and another BitArray
+  ///     let bitArray: BitArray = [true, false, true, false]
+  ///     let secondBitArray = [true, false, false, true]
+  ///     let newBitArray = bitArray ^ secondBitArray
+  ///     print(Array(newBitArray))
+  ///     Prints "[false, false, true, true]"
+  ///
+  /// - Parameters:
+  ///   - lhs: An initialized BitArray
+  ///   - rhs: An initialized BitArray. `rhs` must have same count as `lhs`
+  /// - Returns: The BitArray that is the XOR'd result of the lhs and rhs
   public static func ^ (lhs: BitArray, rhs: BitArray) -> BitArray {
     var copy = lhs
     copy.formBitwiseXor(rhs)
     return copy
   }
-  
+  /// The bitwise NOT operator (`~`) is a prefix operator that returns a value in which all the Bools of its argument are flipped
+  ///
+  /// The following example creates a new a BitArray to be NOT'd with another BitArray
+  ///     let bitArray: BitArray = [true, false, true, false]
+  ///     let newBitArray = ~bitArray
+  ///     print(Array(newBitArray))
+  ///     Prints "[false, true, false, true]"
+  ///
+  /// - Returns: The `BitArray` that contains the inverse or opposite values of that in `self`
   public static prefix func ~ (_ bitArray: BitArray) -> BitArray {
     var copy = bitArray
     copy.formBitwiseNot()
     return copy
   }
-  
+  /// overloaded operator that mutates the lhs BitArray by setting the value in each index to true if either the lhs BitArray or the rhs BitArray -- or both -- have a true value in that respective index.
+  ///
+  /// The following example mutates the lhs to be OR'd with the rhs
+  ///     var bitArray: BitArray = [true, false, true, false]
+  ///     let secondBitArray = [true, false, false, true]
+  ///     bitArray |= secondBitArray
+  ///     print(Array(bitArray))
+  ///     Prints "[true, false, true, true]"
+  ///
+  /// - Parameters:
+  ///   - lhs: An initialized BitArray
+  ///   - rhs: An initialized BitArray. `rhs` must have same count as `lhs`
   public static func |= (lhs: inout BitArray, rhs: BitArray) {
     lhs.formBitwiseOr(rhs)
   }
-  
+  /// overloaded operator that mutates the lhs BitArray by setting the value in each index to true if both the lhs BitArray and the rhs BitArray have a true value in that respective index.
+  ///
+  /// The following example mutates the lhs to be AND'd with the rhs
+  ///     var bitArray: BitArray = [true, false, true, false]
+  ///     let secondBitArray = [true, false, false, true]
+  ///     bitArray &= secondBitArray
+  ///     print(Array(bitArray))
+  ///     Prints "[true, false, false, false]"
+  ///
+  /// - Parameters:
+  ///   - lhs: An initialized BitArray
+  ///   - rhs: An initialized BitArray. `rhs` must have same count as `lhs`
   public static func &= (lhs: inout BitArray, rhs: BitArray) {
     lhs.formBitwiseAnd(rhs)
   }
-  
+  /// overloaded operator that mutates the lhs BitArray by setting the value in each index to true if either the lhs BitArray or the rhs BitArray -- but not both -- have a true value in that respective index.
+  ///
+  /// The following example mutates a BitArray to be XOR'd with another BitArray
+  ///     var bitArray: BitArray = [true, false, true, false]
+  ///     let secondBitArray = [true, false, false, true]
+  ///     bitArray ^= secondBitArray
+  ///     print(Array(bitArray))
+  ///     Prints "[false, false, true, true]"
+  ///
+  /// - Parameters:
+  ///   - lhs: An initialized BitArray
+  ///   - rhs: An initialized BitArray. `rhs` must have same count as `lhs`
   public static func ^= (lhs: inout BitArray, rhs: BitArray) {
     lhs.formBitwiseXor(rhs)
   }
