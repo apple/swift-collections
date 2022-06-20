55f3b584376138d202474e46977d9a1c5990d7ae
Karoy Lorentey <klorentey@apple.com>
2022-06-20 12:54:39 -0700

    [benchmark] Update bit collections module name

--- Benchmarks/Benchmarks/BitsetBenchmarks.swift
+++ Benchmarks/Benchmarks/BitsetBenchmarks.swift
@@ -10,7 +10,7 @@
 //===----------------------------------------------------------------------===//
 
 import CollectionsBenchmark
-import BitArrayModule
+import BitCollections
 
 extension Benchmark {
   
