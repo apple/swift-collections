d9e22947f7c5c77e7d05d10f80f2410670172208
Michael Steindorfer <msteindorfer@apple.com>
2022-08-09 16:30:24 +0200

    [Capsule] Provide skeletons for `keys` and `values` views
    These are simplistic approximations that will require rework.

---
+++ Sources/Capsule/PersistentDictionary+Keys.swift
@@ -0,0 +1,25 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Collections open source project
+//
+// Copyright (c) 2022 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
+
+// TODO: implement a custom `Keys` view rather than relying on an array representation
+extension PersistentDictionary {
+    ///
+    /// A view of a dictionary’s keys.
+    ///
+    public typealias Keys = [Key]
+
+    ///
+    /// A collection containing just the keys of the dictionary.
+    ///
+    public var keys: Self.Keys /* { get } */ {
+        self.map { $0.key }
+    }
+}
