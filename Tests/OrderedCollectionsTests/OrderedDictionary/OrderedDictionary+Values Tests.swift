e42fb868b71eb049f0b842ca2525c35067225e86
Rick van Voorden <rick@northbronson.com>
2023-12-07 16:57:22 -0800

    forward ordered dictionary values equality to values property

--- Tests/OrderedCollectionsTests/OrderedDictionary/OrderedDictionary+Values Tests.swift
+++ Tests/OrderedCollectionsTests/OrderedDictionary/OrderedDictionary+Values Tests.swift
@@ -15,7 +15,40 @@ import XCTest
 import _CollectionsTestSupport
 
 class OrderedDictionaryValueTests: CollectionTestCase {
-  func test_values_getter() {
+  func test_values_getter_equal() {
+    let left: OrderedDictionary = [
+      "one": 1,
+      "two": 2,
+      "three": 3,
+      "four": 4,
+    ]
+    let right: OrderedDictionary = [
+      "one": 1,
+      "two": 2,
+      "three": 3,
+      "four": 4,
+    ]
+    expectEqual(left.values, right.values)
+  }
+  
+  func test_values_getter_not_equal() {
+    let left: OrderedDictionary = [
+      "one": 1,
+      "two": 2,
+      "three": 3,
+      "four": 4,
+    ]
+    let right: OrderedDictionary = [
+      "one": 1,
+      "two": 2,
+      "three": 3,
+      "four": 4,
+      "five": 5,
+    ]
+    expectNotEqual(left.values, right.values)
+  }
+  
+  func test_values_getter_equal_elements() {
     let d: OrderedDictionary = [
       "one": 1,
       "two": 2,
@@ -23,6 +56,16 @@ class OrderedDictionaryValueTests: CollectionTestCase {
       "four": 4,
     ]
     expectEqualElements(d.values, [1, 2, 3, 4])
+  }
+  
+  func test_values_getter_not_equal_elements() {
+    let d: OrderedDictionary = [
+      "one": 1,
+      "two": 2,
+      "three": 3,
+      "four": 4,
+    ]
+    expectNotEqualElements(d.values, [1, 2, 3, 4, 5])
   }
 
   func test_values_RandomAccessCollection() {
