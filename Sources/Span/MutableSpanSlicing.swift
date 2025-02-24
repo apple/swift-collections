e352bf057cca4152c26898a87c04a30c90fdfed2
Guillaume Lessard <guillaume.lessard@apple.com>
2025-02-24 09:11:03 -0800

    adjust copyright years

--- Sources/Span/MutableSpanSlicing.swift
+++ Sources/Span/MutableSpanSlicing.swift
@@ -1,3 +1,15 @@
+//===--- MutableSpanSlicing.swift -----------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2025 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
 //MARK: Option 1 extracting() function
 @available(macOS 9999, *)
 extension MutableSpan where Element: ~Copyable {
