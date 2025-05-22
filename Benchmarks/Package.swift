c50c57f52eaae3f241dd3a4338d05f199d0bd94d
Karoy Lorentey <klorentey@apple.com>
2025-05-21 21:20:52 -0700

    Bump requirements of nested benchmarking package
    - swift-collections-benchmark 0.0.4
    - Swift 6.1 minimum toolchain version
    - macOS 15 minimum deployment target

--- Benchmarks/Package.swift
+++ Benchmarks/Package.swift
@@ -1,9 +1,9 @@
-// swift-tools-version:5.7
+// swift-tools-version:6.1
 //===----------------------------------------------------------------------===//
 //
 // This source file is part of the Swift Collections open source project
 //
-// Copyright (c) 2021 - 2024 Apple Inc. and the Swift project authors
+// Copyright (c) 2021 - 2025 Apple Inc. and the Swift project authors
 // Licensed under Apache License v2.0 with Runtime Library Exception
 //
 // See https://swift.org/LICENSE.txt for license information
@@ -14,13 +14,14 @@ import PackageDescription
 
 let package = Package(
   name: "swift-collections.Benchmarks",
+  platforms: [.macOS(.v15), .iOS(.v18), .watchOS(.v11), .tvOS(.v18), .visionOS(.v2)],
   products: [
     .executable(name: "benchmark", targets: ["benchmark"]),
     .executable(name: "memory-benchmark", targets: ["memory-benchmark"]),
   ],
   dependencies: [
     .package(name: "swift-collections", path: ".."),
-    .package(url: "https://github.com/apple/swift-collections-benchmark", from: "0.0.3"),
+    .package(url: "https://github.com/apple/swift-collections-benchmark", from: "0.0.4"),
   ],
   targets: [
     .target(
