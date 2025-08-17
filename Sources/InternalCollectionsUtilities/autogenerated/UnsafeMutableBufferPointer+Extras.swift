b6050e36435a9cd7bca7136c63ba03aeb11176c1
Karoy Lorentey <klorentey@apple.com>
2025-08-16 21:39:10 -0700

    Add _isIdentical, _extracting(first:)/_extracting(last:) as UnsafeBufferPointer extensions

--- Sources/InternalCollectionsUtilities/autogenerated/UnsafeMutableBufferPointer+Extras.swift
+++ Sources/InternalCollectionsUtilities/autogenerated/UnsafeMutableBufferPointer+Extras.swift
@@ -2,7 +2,7 @@
 //
 // This source file is part of the Swift Collections open source project
 //
-// Copyright (c) 2022 - 2024 Apple Inc. and the Swift project authors
+// Copyright (c) 2022 - 2025 Apple Inc. and the Swift project authors
 // Licensed under Apache License v2.0 with Runtime Library Exception
 //
 // See https://swift.org/LICENSE.txt for license information
@@ -22,6 +22,63 @@
 // but in regular builds we want them to be public. Unfortunately
 // the current best way to do this is to duplicate all definitions.
 #if COLLECTIONS_SINGLE_MODULE
+extension UnsafeMutableBufferPointer where Element: ~Copyable {
+  /// Returns a Boolean value indicating whether two
+  /// `UnsafeMutableBufferPointer` instances refer to the same region in
+  /// memory.
+  @inlinable @inline(__always)
+  internal func _isIdentical(to other: Self) -> Bool {
+    (self.baseAddress == other.baseAddress) && (self.count == other.count)
+  }
+}
+
+extension UnsafeMutableBufferPointer where Element: ~Copyable {
+  /// Returns a buffer pointer containing the initial elements of this buffer,
+  /// up to the specified maximum length.
+  ///
+  /// If the maximum length exceeds the length of this buffer pointer,
+  /// then the result contains all the elements.
+  ///
+  /// The returned buffer's first item is always at offset 0; unlike buffer
+  /// slices, extracted buffers do not share their indices with the
+  /// buffer from which they are extracted.
+  ///
+  /// - Parameter maxLength: The maximum number of elements to return.
+  ///   `maxLength` must be greater than or equal to zero.
+  /// - Returns: A buffer pointer with at most `maxLength` elements.
+  ///
+  /// - Complexity: O(1)
+  @_alwaysEmitIntoClient
+  @inline(__always)
+  public func _extracting(first maxLength: Int) -> Self {
+    precondition(maxLength >= 0, "Can't have a prefix of negative length")
+    let newCount = Swift.min(maxLength, count)
+    return Self(start: baseAddress, count: newCount)
+  }
+
+  /// Returns a buffer pointer containing the final elements of this buffer,
+  /// up to the given maximum length.
+  ///
+  /// If the maximum length exceeds the length of this buffer pointer,
+  /// the result contains all the elements.
+  ///
+  /// The returned buffer's first item is always at offset 0; unlike buffer
+  /// slices, extracted buffers do not share their indices with the
+  /// buffer from which they are extracted.
+  ///
+  /// - Parameter maxLength: The maximum number of elements to return.
+  ///   `maxLength` must be greater than or equal to zero.
+  /// - Returns: A buffer pointer with at most `maxLength` elements.
+  ///
+  /// - Complexity: O(1)
+  @_alwaysEmitIntoClient
+  @inline(__always)
+  public func _extracting(last maxLength: Int) -> Self {
+    precondition(maxLength >= 0, "Can't have a suffix of negative length")
+    let newCount = Swift.min(maxLength, count)
+    return extracting(Range(uncheckedBounds: (count - newCount, count)))
+  }
+}
 extension UnsafeMutableBufferPointer {
   @inlinable
   internal func initialize(fromContentsOf source: Self) -> Index {
@@ -149,6 +206,63 @@ extension Slice {
   }
 }
 #else // !COLLECTIONS_SINGLE_MODULE
+extension UnsafeMutableBufferPointer where Element: ~Copyable {
+  /// Returns a Boolean value indicating whether two
+  /// `UnsafeMutableBufferPointer` instances refer to the same region in
+  /// memory.
+  @inlinable @inline(__always)
+  public func _isIdentical(to other: Self) -> Bool {
+    (self.baseAddress == other.baseAddress) && (self.count == other.count)
+  }
+}
+
+extension UnsafeMutableBufferPointer where Element: ~Copyable {
+  /// Returns a buffer pointer containing the initial elements of this buffer,
+  /// up to the specified maximum length.
+  ///
+  /// If the maximum length exceeds the length of this buffer pointer,
+  /// then the result contains all the elements.
+  ///
+  /// The returned buffer's first item is always at offset 0; unlike buffer
+  /// slices, extracted buffers do not share their indices with the
+  /// buffer from which they are extracted.
+  ///
+  /// - Parameter maxLength: The maximum number of elements to return.
+  ///   `maxLength` must be greater than or equal to zero.
+  /// - Returns: A buffer pointer with at most `maxLength` elements.
+  ///
+  /// - Complexity: O(1)
+  @_alwaysEmitIntoClient
+  @inline(__always)
+  public func _extracting(first maxLength: Int) -> Self {
+    precondition(maxLength >= 0, "Can't have a prefix of negative length")
+    let newCount = Swift.min(maxLength, count)
+    return Self(start: baseAddress, count: newCount)
+  }
+
+  /// Returns a buffer pointer containing the final elements of this buffer,
+  /// up to the given maximum length.
+  ///
+  /// If the maximum length exceeds the length of this buffer pointer,
+  /// the result contains all the elements.
+  ///
+  /// The returned buffer's first item is always at offset 0; unlike buffer
+  /// slices, extracted buffers do not share their indices with the
+  /// buffer from which they are extracted.
+  ///
+  /// - Parameter maxLength: The maximum number of elements to return.
+  ///   `maxLength` must be greater than or equal to zero.
+  /// - Returns: A buffer pointer with at most `maxLength` elements.
+  ///
+  /// - Complexity: O(1)
+  @_alwaysEmitIntoClient
+  @inline(__always)
+  public func _extracting(last maxLength: Int) -> Self {
+    precondition(maxLength >= 0, "Can't have a suffix of negative length")
+    let newCount = Swift.min(maxLength, count)
+    return extracting(Range(uncheckedBounds: (count - newCount, count)))
+  }
+}
 extension UnsafeMutableBufferPointer {
   @inlinable
   public func initialize(fromContentsOf source: Self) -> Index {
