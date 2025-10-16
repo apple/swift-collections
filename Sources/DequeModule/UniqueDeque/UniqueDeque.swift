de68a98a82cb29b5339e3a3667e7f16cab9c261d
Alejandro Alonso <alejandro_alonso@apple.com>
2025-10-16 14:45:22 -0700

    wip

---
+++ Sources/DequeModule/UniqueDeque/UniqueDeque.swift
@@ -0,0 +1,39 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Collections open source project
+//
+// Copyright (c) 2025 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
+
+#if !COLLECTIONS_SINGLE_MODULE
+import InternalCollectionsUtilities
+import ContainersPreview
+#endif
+
+#if compiler(<6.2)
+
+@frozen
+@available(*, unavailable, message: "RigidDeque requires a Swift 6.2 toolchain")
+public struct RigidDeque<Element: ~Copyable>: ~Copyable {
+  public init() {
+    fatalError()
+  }
+}
+
+#else
+
+@available(SwiftStdlib 5.0, *)
+@safe
+@frozen
+public struct UniqueDeque<Element: ~Copyable>: ~Copyable {
+
+}
+
+@available(SwiftStdlib 5.0, *)
+extension UniqueDeque: @unchecked Sendable where Element: Sendable & ~Copyable {}
+
+#endif // compiler(<6.2)
