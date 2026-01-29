942cfbfd68126875e91bce86d7973cbf67e364c8
Guillaume Lessard <guillaume.lessard@apple.com>
2026-01-28 17:28:15 -0800

    Add temporary workaround for broken OutputSpan.wUMBP in current toolchain snapshots
    (cherry picked from commit 0b1ff87d6773fe568945d8af86a236dbbf9157d9)

--- Sources/BasicContainers/RigidArray+Insertions.swift
+++ Sources/BasicContainers/RigidArray+Insertions.swift
@@ -251,6 +251,31 @@ extension RigidArray where Element: ~Copyable {
 }
 
 @available(SwiftStdlib 5.0, *)
+extension OutputSpan where Element: ~Copyable {
+  @_alwaysEmitIntoClient
+  @_lifetime(self: copy self)
+  mutating func _withUnsafeMutableBufferPointer<E: Error, R: ~Copyable>(
+    _ body: (
+      UnsafeMutableBufferPointer<Element>,
+      _ initializedCount: inout Int
+    ) throws(E) -> R
+  ) throws(E) -> R {
+    // FIXME: Work around rdar://169036911
+    let capacity = self.capacity
+    let baseAddress = self.span.withUnsafeBufferPointer { $0.baseAddress } // Wow.
+    return try withUnsafeMutableBufferPointer { buffer, count throws(E) in
+      let correctedBuffer = UnsafeMutableRawBufferPointer(
+        start: .init(mutating: baseAddress), // Wow, wow.
+        count: capacity &* MemoryLayout<Element>.stride)
+      return try correctedBuffer.withMemoryRebound(to: Element.self) {
+        correctBuffer throws(E) in
+        try body(correctBuffer, &count)
+      }
+    }
+  }
+}
+
+@available(SwiftStdlib 5.0, *)
 extension RigidArray {
   /// Copies the elements of a fully initialized buffer pointer into this
   /// array at the specified position.
