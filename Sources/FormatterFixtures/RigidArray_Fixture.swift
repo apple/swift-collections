d88fb1ab89806135a925aa33d489ab8beb8b78fb
Dave Lee <davelee.com@gmail.com>
2026-04-30 12:51:13 -0700

    Add license header to RigidArray_Fixture.swift

--- Sources/FormatterFixtures/RigidArray_Fixture.swift
+++ Sources/FormatterFixtures/RigidArray_Fixture.swift
@@ -1,3 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Collections open source project
+//
+// Copyright (c) YEARS Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+// SPDX-License-Identifier: Apache-2.0 WITH Swift-exception
+//
+//===----------------------------------------------------------------------===//
+
 import BasicContainers
 
 @available(SwiftStdlib 5.0, *)
