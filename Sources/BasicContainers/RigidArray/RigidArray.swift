eb5c24f71a3d449e7ccec5fe896c4524843e0ea5
Alejandro Alonso <alejandro_alonso@apple.com>
2026-06-10 14:32:06 -0700

    Update Sources/BasicContainers/RigidArray/RigidArray.swift
    Co-authored-by: Karoy Lorentey <klorentey@apple.com>

--- Sources/BasicContainers/RigidArray/RigidArray.swift
+++ Sources/BasicContainers/RigidArray/RigidArray.swift
@@ -345,7 +345,8 @@ extension RigidArray where Element: ~Copyable {
   /// to its new storage. The old storage is then deallocated.
   ///
   /// - Parameter newCapacity: The desired new capacity. `newCapacity` must be
-  ///    greater than or equal to the current count.
+/// - Parameter newCapacity: The desired new capacity. The new capacity
+///    is set to the maximum of `newCapacity` and the current count.
   ///
   /// - Complexity: O(`count`)
   @inlinable
