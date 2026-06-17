f37fe8e662682ba96dc94962e059f921fcc82e24
Karoy Lorentey <klorentey@apple.com>
2026-06-17 14:57:06 -0700

    Define SwiftStdlib 6.4 with actual version numbers

--- Package.swift
+++ Package.swift
@@ -104,7 +104,7 @@ let availabilityMacros: KeyValuePairs<String, String> = [
   "SwiftStdlib 6.1":  "macOS 15.4, iOS 18.4, watchOS 11.4, tvOS 18.4, visionOS 2.4",
   "SwiftStdlib 6.2":  "macOS 26.0, iOS 26.0, watchOS 26.0, tvOS 26.0, visionOS 26.0",
   "SwiftStdlib 6.3":  "macOS 26.4, iOS 26.4, watchOS 26.4, tvOS 26.4, visionOS 26.4",
-  "SwiftStdlib 6.4":  "macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, visionOS 9999",
+  "SwiftStdlib 6.4":  "macOS 27.0, iOS 27.0, watchOS 27.0, tvOS 27.0, visionOS 27.0",
   "SwiftStdlib 6.5":  "macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, visionOS 9999",
   // Note: if you touch these, please make sure to also update the similar lists in
   // CMakeLists.txt and Xcode/Shared.xcconfig.
