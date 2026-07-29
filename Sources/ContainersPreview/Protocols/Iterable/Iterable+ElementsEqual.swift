fbe36a7af591d5c5948d30cec4fad0ec94d0cfb1
Karoy Lorentey <klorentey@apple.com>
2026-07-28 17:06:49 -0700

    Rearrange Sources/ContainersPreview/Protocols/*

---
+++ Sources/ContainersPreview/Protocols/Iterable/Iterable+ElementsEqual.swift
@@ -0,0 +1,66 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Collections open source project
+//
+// Copyright (c) 2026 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+// SPDX-License-Identifier: Apache-2.0 WITH Swift-exception
+//
+//===----------------------------------------------------------------------===//
+
+#if compiler(>=6.4) && UnstableContainersPreview
+
+@available(SwiftStdlib 6.4, *)
+extension Iterable
+  where Self: ~Copyable & ~Escapable, Element: ~Copyable
+{
+  @inlinable
+  public func elementsEqual<OtherSequence: Iterable>(
+    _ other: borrowing OtherSequence,
+    by areEquivalent: (borrowing Element, borrowing OtherSequence.Element) throws(Failure) -> Bool
+  ) throws(Failure) -> Bool
+  where OtherSequence: ~Copyable & ~Escapable, OtherSequence.Element: ~Copyable, OtherSequence.Failure == Failure
+  {
+    // FIXME: Forward to the iterator's implementation of same
+    var iter1 = makeBorrowingIterator()
+    var iter2 = other.makeBorrowingIterator()
+    while true {
+      var el1 = try iter1.nextSpan()
+
+      if el1.isEmpty {
+        // LHS is empty - sequences are equal iff RHS is also empty
+        let el2 = try iter2.nextSpan(maxCount: 1)
+        return el2.isEmpty
+      }
+
+      while el1.count > 0 {
+        let el2 = try iter2.nextSpan(maxCount: el1.count)
+        if el2.isEmpty { return false }
+        for i in 0..<el2.count {
+          if try !areEquivalent(el1[i], el2[i]) { return false }
+        }
+        el1 = el1.extracting(droppingFirst: el2.count)
+      }
+    }
+  }
+}
+
+@available(SwiftStdlib 6.4, *)
+extension Iterable
+where Self: ~Copyable & ~Escapable, Element: ~Copyable & Equatable {
+  @inlinable
+  public func elementsEqual<OtherSequence: Iterable<Element, Failure>>(
+    _ other: borrowing OtherSequence
+  ) throws(Failure) -> Bool
+  where
+    OtherSequence: ~Copyable & ~Escapable,
+    OtherSequence.Element: ~Copyable
+  {
+    return try self.elementsEqual(other, by: ==)
+  }
+}
+
+#endif
